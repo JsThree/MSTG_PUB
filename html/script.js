5991f6ba5b6a19ee55122034f0268f62a8ffc8bf
Trent Greek <95935752+JsThree@users.noreply.github.com>
2022-12-16 15:50:07 +0000

    asd

---
+++ html/script.js
@@ -0,0 +1,63 @@
+var socket;
+var usernameInput
+var chatIDInput;
+var messageInput;
+var chatRoom;
+var dingSound;
+var messages = [];
+var delay = true;
+
+function onload(){
+  socket = io();
+  usernameInput = document.getElementById("NameInput");
+  chatIDInput = document.getElementById("IDInput");
+  messageInput = document.getElementById("ComposedMessage");
+  chatRoom = document.getElementById("RoomID");
+  dingSound = document.getElementById("Ding");
+
+  socket.on("join", function(room){
+    chatRoom.innerHTML = "Chatroom : " + room;
+  })
+  socket.on("clear", function() {
+    console.log("clear")
+  })
+
+  socket.on("recieve", function(message){
+    console.log(message);
+    if (messages.length < 30){
+      messages.push(message);
+      dingSound.currentTime = 0;
+      dingSound.play();
+    }
+    else{
+      messages.shift();
+      messages.push(message);
+    }
+    for (i = 0; i < messages.length; i++){
+        document.getElementById("Message"+i).innerHTML = messages[i];
+        document.getElementById("Message"+i).style.color = "#303030";
+    }
+  })
+}
+
+function Connect(){
+  socket.emit("join", chatIDInput.value, usernameInput.value);
+}
+
+function Send(){
+  if (delay && messageInput.value.replace(/\s/g, "") != ""){
+    delay = false;
+    setTimeout(delayReset, 35);
+    socket.emit("send", messageInput.value);
+    messageInput.value = "";
+  }
+}
+document.addEventListener("keydown", function(e){
+  console.log(e.key)
+  if (e.key == "Enter"){
+    Send();
+  }
+});
+function delayReset(){
+  delay = true;
+}
